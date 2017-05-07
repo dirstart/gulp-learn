dac80d9b884ca553b5932e609f3e159775d35bb4
zhuqianye <q835041058@gmail.com>
2017-05-07 18:57:03 +0800

    gulp test

---
+++ gulp-first/gulpfile.js
@@ -0,0 +1,23 @@
+var gulp = require('gulp'),
+	uglify = require("gulp-uglify");
+
+gulp.task('default',function(){
+	console.log("hello gulp");
+});
+gulp.task('minify-js',function(){
+	gulp.src('js/*.js')//要压缩的js路径和文件
+	.pipe(uglify())
+	.pipe(gulp.dest('dist/js'));//压缩后的路径
+});
+
+// gulp.task('task1',function(){
+// 	console.log("1 finish");
+// });
+// gulp.task('task2',function(){
+// 	console.log("task2 finish");
+// });
+
+// gulp.task('end',['task1','task2'],function(){
+// 	console.log("all finsih");
+// })
+
